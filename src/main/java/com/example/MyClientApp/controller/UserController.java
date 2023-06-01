b06b652b255a9cf7558a8d31a12a62efbebace7f
Elmaddin Sariyev <elmeddin.sv@gmail.com>
2023-06-01 12:23:48 +0400

    furuza

--- src/main/java/com/example/MyClientApp/controller/UserController.java
+++ src/main/java/com/example/MyClientApp/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.MyClientApp.controller;
 
-
 import com.example.MyClientApp.dto.UserDto;
 import com.example.MyClientApp.request.UserChangePassword;
 import com.example.MyClientApp.request.UserRequest;
 import com.example.MyClientApp.service.UserService;
+import com.example.MyClientApp.util.TokenGenerator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +20,11 @@ public class UserController {
     public ResponseEntity<UserDto> updateUser(@RequestBody UserRequest request) {
         return ResponseEntity.ok().body(userService.updateUser(request));
     }
-    
+
     @GetMapping("/getUserById")
     public ResponseEntity<UserDto> getUserById() {
         return ResponseEntity.ok().body(userService.findUserById());
     }
-
 
     @PatchMapping("/editPassword")
     public ResponseEntity<Void> updateUserPassword(@RequestBody UserChangePassword request) {
