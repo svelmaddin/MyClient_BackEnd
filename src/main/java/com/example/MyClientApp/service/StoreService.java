5133bce901d8de269ca2704c16e3c30fb70da731
Elmaddin Sariyev <elmeddin.sv@gmail.com>
2023-08-17 16:58:07 +0400

    **

--- src/main/java/com/example/MyClientApp/service/StoreService.java
+++ src/main/java/com/example/MyClientApp/service/StoreService.java
@@ -8,17 +8,11 @@ import com.example.MyClientApp.model.User;
 import com.example.MyClientApp.repository.UserRepository;
 import com.example.MyClientApp.request.CreateShopRequest;
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.Max;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.example.MyClientApp.util.ErrorMessage.USER_NOT_FOUND;
 
@@ -68,7 +62,6 @@ public class StoreService {
     @Transactional
     private void createStoreDetails(StoreDetailsModel request) {
         final String URL = "http://localhost:9090/store/createStore";
-        RestTemplate restTemplate = new RestTemplate();
         HttpEntity<StoreDetailsModel> entity = new HttpEntity<>(
                 new StoreDetailsModel(request.getStoreName(), request.getCountry(), request.getCity(),
                         request.getStreet(), request.getZipcode(), request.getAddress(), request.getPhoneNumber())
